cb65d88515935de5bdffc0f30c90a4ae0b502e05
Tomoki Imai <tomo832@gmail.com>
2018-09-05 22:08:08 +0900

    Add more rects

--- src/rect.h
+++ src/rect.h
@@ -4,8 +4,8 @@
 
 class xy_rect : public hitable {
 public:
-    xy_rect(float _x0, float _y0, float _x1, float _y1, float _z, material* mat)
-        : x0(_x0), y0(_y0), x1(_x1), y1(_y1), z(_z), mat_ptr(mat) { }
+    xy_rect(float _x0, float _x1, float _y0, float _y1, float _z, material* mat)
+        : x0(_x0), x1(_x1), y0(_y0), y1(_y1), z(_z), mat_ptr(mat) { }
 
     bool hit(const ray& r, float t_min, float t_max, hit_record& rec) const {
         float t = (z - r.origin().z()) / r.direction().z();
@@ -32,4 +32,68 @@ public:
 
     material* mat_ptr;
     float x0, y0, x1, y1, z;
+};
+
+class xz_rect : public hitable {
+public:
+    xz_rect(float _x0, float _x1, float _z0, float _z1, float _y, material* mat)
+        : x0(_x0), z0(_z0), x1(_x1), z1(_z1), y(_y), mat_ptr(mat) { }
+
+    bool hit(const ray& r, float t_min, float t_max, hit_record& rec) const {
+        float t = (y - r.origin().y()) / r.direction().y();
+        if (t < t_min || t_max < t)
+            return false;
+        float x = r.origin().x() + t * r.direction().x();
+        float z = r.origin().z() + t * r.direction().z();
+        if (x < x0 || x1 < x || z < z0 || z1 < z)
+            return false;
+
+        rec.u = (x - x0) / (x1 - x0);
+        rec.v = (z - z0) / (z1 - z0);
+        rec.t = t;
+        rec.mat_ptr = mat_ptr;
+        rec.p = r.point_at_parameter(t);
+        rec.normal = vec3(0, 1, 0);
+        return true;
+    }
+
+    bool bounding_box(float t0, float t1, aabb& box) const {
+        box = aabb(vec3(x0, y - 0.0001, z0), vec3(x1, y + 0.0001, z1));
+        return true;
+    }
+
+    material* mat_ptr;
+    float x0, z0, x1, z1, y;
+};
+
+class yz_rect : public hitable {
+public:
+    yz_rect(float _y0, float _y1, float _z0, float _z1, float _x, material* mat)
+        : y0(_y0), z0(_z0), y1(_y1), z1(_z1), x(_x), mat_ptr(mat) { }
+
+    bool hit(const ray& r, float t_min, float t_max, hit_record& rec) const {
+        float t = (x - r.origin().x()) / r.direction().x();
+        if (t < t_min || t_max < t)
+            return false;
+        float y = r.origin().y() + t * r.direction().y();
+        float z = r.origin().z() + t * r.direction().z();
+        if (y < y0 || y1 < y || z < z0 || z1 < z)
+            return false;
+
+        rec.u = (y - y0) / (y1 - y0);
+        rec.v = (z - z0) / (z1 - z0);
+        rec.t = t;
+        rec.mat_ptr = mat_ptr;
+        rec.p = r.point_at_parameter(t);
+        rec.normal = vec3(1, 0, 0);
+        return true;
+    }
+
+    bool bounding_box(float t0, float t1, aabb& box) const {
+        box = aabb(vec3(x - 0.0001, y0, z0), vec3(x + 0.0001, y1, z1));
+        return true;
+    }
+
+    material* mat_ptr;
+    float y0, z0, y1, z1, x;
 };
